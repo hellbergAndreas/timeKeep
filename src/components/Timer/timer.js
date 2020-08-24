e16b5c18c8263d071e8dfbc0800a40fba6987187
Andreas <Hellberg.andreas@gmail.com>
2020-08-24 10:40:38 +0200

    Timer
    timer button starts and stops timer, and creates a setinterval for current category and subcategory

--- src/components/Timer/timer.js
+++ src/components/Timer/timer.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react"
+import React, { useEffect, useState } from "react"
 import Button, { ButtonShape } from "../Button/Button"
 import { connect } from "react-redux"
 import { addTime } from "../../redux/timeReducer/time.action"
@@ -6,14 +6,25 @@ import { addTime } from "../../redux/timeReducer/time.action"
 import styles from "./timer.module.css"
 
 const Timer = ({ category: { subCategory, activeCategory }, addTime }) => {
-  const handleClick = () => {
-    const running = false
-    const addTimeInterval = setInterval(() => {
-      addTime({ activeCategory, subCategory, time: 1 })
-    }, 1000)
-    if (!running) {
-      addTimeInterval()
+  const [intervalId, setIntervalId] = useState(null)
+  const [running, setRunning] = useState(false)
+
+  useEffect(() => {
+    if (running) {
+      setIntervalId(
+        setInterval(
+          () => addTime({ activeCategory, subCategory, time: 1 }),
+          1000
+        )
+      )
+    } else {
+      clearInterval(intervalId)
+      console.log("stoppint interval")
     }
+  }, [running])
+
+  const handleClick = () => {
+    setRunning(!running)
   }
   return (
     <div className={styles.timer}>
